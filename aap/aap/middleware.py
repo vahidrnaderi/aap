9ad1814f194fa9abce374e8ce48edd2a095b7bcb
Morteza NourelahiAlamdari <m@0t1.me>
2021-12-01 21:57:13 +0100

    Update CORS middleware

--- aap/aap/middleware.py
+++ aap/aap/middleware.py
@@ -11,5 +11,5 @@ class CorsMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        response["access-control-allow-origin"] = settings.CORS_ALLOWED_HOST
+        response["Access-Control-Allow-Origin"] = settings.CORS_ALLOWED_HOST
         return response
